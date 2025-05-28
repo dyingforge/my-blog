f1133b68ebdb07d6ed9f282d2b7b765819d6ac05
dyingforge <1289470395@qq.com>
2025-05-28 14:51:02 +0800

    [feat] init

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="container">
+      <h1 className = 'text-red-500'>Welcome to My Blog</h1>
+      <p className='text-gray-700'>This is the homepage of my blog where I share my thoughts and experiences.</p>
+    </div>
+  );
+}
